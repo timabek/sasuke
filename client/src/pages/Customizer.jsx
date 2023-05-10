981b214aec1b12d8124c8ebee88d5b2be0b2451f
timaDev00 <timabeknarynbekovich@gmail.com>
2023-05-10 19:39:28 +0600

    add this

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://shirts-hzvf.onrender.com/api/v1/dalle', {
+      const response = await fetch('https://sasuke-vxd9.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
